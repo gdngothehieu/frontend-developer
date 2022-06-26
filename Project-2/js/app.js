1442d7b3d6254e0cd747a27beaf735fb800730e4
HieuNT114 <hieunt114@fsoft.com.vn>
2022-06-26 12:55:44 +0700

    Update

---
+++ Project-2/js/app.js
@@ -0,0 +1,12 @@
+let sectionNav = document.querySelectorAll("section");
+let NavTag = document.getElementById("navbar_header");
+let sectionNavLength = sectionNav.length;
+let sectionNavPositions = [];
+let prePosition = 0;
+let currentPosition = 0;
+
+const initiateNavBar = () => {
+  for (let i = 0; i < sectionNav.length; i++) {}
+};
+
+initiateNavBar();
