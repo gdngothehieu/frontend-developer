463a0c87c7517c8811510bbbd95d8315c508727b
Ngo The Hieu <cusaniv@gmail.com>
2022-08-29 20:22:35 +0700

    Update

--- FEND-Capstone-Travel-App/__test__/client.test.js
+++ FEND-Capstone-Travel-App/__test__/client.test.js
@@ -1,7 +1,16 @@
-import { handleSubmit } from "../src/client/js/app";
+import {
+  handleSubmit,
+  getWeatherBitData,
+  getPixabayImages,
+  getGeonameData,
+} from "../src/client/js/app";
 
 describe("handleSubmit()", () => {
   test("It should success", () => {
     expect(handleSubmit).toBeDefined();
+    expect(getPixabayImages).toBeDefined();
+
+    expect(getWeatherBitData).toBeDefined();
+    expect(getGeonameData).toBeDefined();
   });
 });
